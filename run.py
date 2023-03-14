131bd2e1656d0a93bcc2b143dbc5044e6485e615
Hesam Ossanloo <hesam.ossanloo@gmail.com>
2023-03-14 20:00:08 +0100

    accessing secret manager

--- run.py
+++ run.py
@@ -4,27 +4,30 @@ import os
 from flask import Flask
 from flask import request
 from markupsafe import escape
+from google.cloud import secretmanager
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Welcome to Volpes Energy. The API is working. Yay!'
+    return "Welcome to Volpes Energy. The API is working. Yay!"
 
 
-@app.route('/marketdata')
+@app.route("/marketdata")
 def get_source():
-    market_data_type = request.args.get('type', 'not_provided')
-    data = 'the provided query string, type={}, is not supported'.format(escape(market_data_type))
+    market_data_type = request.args.get("type", "not_provided")
+    data = "the provided query string, type={}, is not supported".format(escape(market_data_type))
+
+    secrets = secretmanager.SecretManagerServiceClient()
+    SHOWING_WE_KNOW_HOW_TO_USE_SM = secrets.get_secret(request={"name": "projects/115358684500/secrets/SOMETHING_IMPORTANT/versions/1"})
 
     f = open("mockdata/day-ahead.json", "rb")
     json_object = json.load(f)
     f.close()
 
     if not market_data_type:
-        data = 'the type query parameter is not provided'
-    elif market_data_type == 'day-ahead':
+        data = "the type query parameter is not provided. Hint: {}".format((SHOWING_WE_KNOW_HOW_TO_USE_SM.response.payload.data.decode("UTF-8")))
+    elif market_data_type == "day-ahead":
         data = json.dumps(json_object)
     return data
 
