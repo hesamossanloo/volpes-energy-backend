4a4bf4c463e06e45069b4a728e58c255db34d271
Hesam Ossanloo <hesam.ossanloo@gmail.com>
2023-03-15 14:37:51 +0100

    clean up the ortools and other non-necessary dependencies

--- run.py
+++ run.py
@@ -234,42 +234,5 @@ def ev_optimal_dispatch():
 
     return df.to_json()
 
-
-@app.route("/ortools_test")
-def ortoolstest():
-    # Create the linear solver with the GLOP backend.
-    solver = pywraplp.Solver.CreateSolver('GLOP')
-    if not solver:
-        return
-
-    # Create the variables x and y.
-    x = solver.NumVar(0, 1, 'x')
-    y = solver.NumVar(0, 2, 'y')
-
-    print('Number of variables =', solver.NumVariables())
-
-    # Create a linear constraint, 0 <= x + y <= 2.
-    ct = solver.Constraint(0, 2, 'ct')
-    ct.SetCoefficient(x, 1)
-    ct.SetCoefficient(y, 1)
-
-    print('Number of constraints =', solver.NumConstraints())
-
-    # Create the objective function, 3 * x + y.
-    objective = solver.Objective()
-    objective.SetCoefficient(x, 3)
-    objective.SetCoefficient(y, 1)
-    objective.SetMaximization()
-
-    solver.Solve()
-
-    print('Solution:')
-    print('Objective value =', objective.Value())
-    print('x =', x.solution_value())
-    print('y =', y.solution_value())
-
-    return '{}, {}'.format(x.solution_value(), y.solution_value())
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
